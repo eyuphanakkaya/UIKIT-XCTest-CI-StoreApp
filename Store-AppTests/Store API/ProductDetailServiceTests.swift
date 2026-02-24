5b1228b1ce72018f709876ccd32b477f8cde7f7a
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-02-24 15:06:57 +0300

    load requests data from url and create helper for valid response in 'ProductDetailServiceTests'

--- Store-AppTests/Store API/ProductDetailServiceTests.swift
+++ Store-AppTests/Store API/ProductDetailServiceTests.swift
@@ -18,11 +18,36 @@ final class ProductDetailServiceTests: XCTestCase {
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
+    func test_load_requestsDataFromURL() async throws {
+        let url = URL(string: "https://a-url.com")!
+        let (sut, client) = makeSUT(result: .success(validProductDetailResponse()), url: url)
+        
+        _ = try await sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(result: Result<(Data, HTTPURLResponse), Error>, url: URL = .init(string: "https://example.com")!) -> (ProductDetailService, HTTPClientSpy) {
         let client = HTTPClientSpy(result: result)
         let sut = ProductDetailService(client: client, url: url)
         return (sut, client)
+    }
+    
+    
+    func validProductDetailResponse() -> (Data, HTTPURLResponse) {
+        let json = """
+        {
+            "id": 1,
+            "title": "Test Product",
+            "price": 10.0,
+            "description": "Test Description",
+            "category": "Test Category",
+            "image": "https://image.com/test.png"
+        }
+        """.data(using: .utf8)!
+        
+        return (json, anyHttpResponse(statusCode: 200))
     }
 }
