be037d8205db51722869519912bf2469aecdc9fd
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-02-27 16:36:00 +0300

    remove comment lines

--- StoreServicesAPIEndToEndTests/ProductDetailMapperAPIEndToEndTests.swift
+++ StoreServicesAPIEndToEndTests/ProductDetailMapperAPIEndToEndTests.swift
@@ -41,5 +41,3 @@ final class ProductDetailMapperAPIEndToEndTests: XCTestCase {
     }
 
 }
-//https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png
-//https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png
